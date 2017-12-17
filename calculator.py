0791c1b5a59e49116a4d72230d0db0d4ddd15550
neatcat <neatcat@163.com>
2017-12-17 22:04:13 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -11,7 +11,8 @@ PIT_RATE = 0
 #
 def SALARY_CAL(x,y,z):
     PIT_DATA = x * y - z
-    return format(PIT_DATA,".2f")
+    #return format(PIT_DATA,".2f")
+    return PIT_DATA
 
 def PIT_CAL(value):
     #value = 1500
@@ -31,15 +32,26 @@ def PIT_CAL(value):
         return SALARY_CAL(value,0.45,13505)
 
 try:
-    PER_SALARY = int(sys.argv[1])
-    if PER_SALARY > 0:
-        #print(PER_SALARY)
-        if PER_SALARY <= PIT_EV:
-            print(format(PER_SALARY,".2f"))
+    ARGV_LIST = sys.argv[1:]
+    if not ARGV_LIST:
+        #print("blank parameter")
+        bypass
+    #print(ARGV_LIST)
+    for argv in ARGV_LIST:
+        USER_LIST = argv.split(':')   
+        USERID = int(USER_LIST[0])
+        SALARY = int(USER_LIST[1])
+        #PER_SALARY = format(PER_SALARY,".2f")
+        #print("%d:%s" % (USERID,format(PER_SALARY,".2f")))
+        if SALARY > 0:
+            PER_SALARY = SALARY * (1 - 0.08 - 0.02 - 0.005 - 0.06)
+            #print(PER_SALARY)
+            if PER_SALARY <= PIT_EV:
+                print("%d:%s" % (USERID,format(PER_SALARY,".2f")))
+            else:
+                print("%d:%s" % (USERID,format(PER_SALARY - PIT_CAL(PER_SALARY - PIT_EV),".2f")))
         else:
-            print(PIT_CAL(PER_SALARY - PIT_EV))
-    else:
-        print("""Parameter Error!!!
+                print("""Parameter Error!!!
 The salary must be a positive number.""")
 except:
     print("Parameter Error")
